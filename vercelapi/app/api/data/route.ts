db9774d88abbcad320a6b14d643235dec3a18dc1
financegeek-org <nlai@rediffmail.com>
2024-10-19 17:14:42 -0700

    data - error scenario

--- vercelapi/app/api/data/route.ts
+++ vercelapi/app/api/data/route.ts
@@ -1,17 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+const responseJson = {
+  "credits": 1000,
+  "data": `Jeff works as Software Engineer at Apple.
+John works as Software Engineer at Facebook.
+Steve works as Sales at Facebook.
+Ivan works as Sales at Apple.
+Zhang works as Software Engineer at Facebook.
+Chen works as Software Engineer at Facebook.
+Zhao works as Software Engineer at Facebook.
+`,
+}
+const responseJsonError = {
+  "credits": 3,
+}
+
+
 export async function POST(request: NextRequest) {
   try {
     // Parse the JSON body from the request
     const body = await request.json();
-    console.log(body.content);
-    const responseJson = {
-      "msg": "Hello World",
+    if (body.key==="secretKey") {
+      // Return the response as JSON
+      console.log('response',responseJson);
+      return NextResponse.json(responseJson)
     }
-
-    // Return the response as JSON
-    console.log('response',responseJson);
-    return NextResponse.json(responseJson)
+    else {
+      return NextResponse.json(responseJsonError)
+    }
   } catch (error) {
     // If there's an error, return a 400 Bad Request response
     return NextResponse.json({ error: 'Invalid JSON input' }, { status: 400 })
@@ -20,11 +36,6 @@ export async function POST(request: NextRequest) {
 
 export async function GET() {
   try {
-    const responseJson = {
-      "credits": 1000,
-      "data": "",
-    }
-
     // Return the response as JSON
     console.log('response',responseJson);
     return NextResponse.json(responseJson)
