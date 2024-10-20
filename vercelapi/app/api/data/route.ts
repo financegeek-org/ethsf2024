d397ffeb7266e727a39538f77addcadd0e9b63ca
financegeek-org <nlai@rediffmail.com>
2024-10-20 06:53:22 -0700

    route - data

--- vercelapi/app/api/data/route.ts
+++ vercelapi/app/api/data/route.ts
@@ -1,15 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const responseJson = {
-  "credits": 1000,
-  "data": `Jeff works as Software Engineer at Apple.
+const data = `Jeff works as Software Engineer at Apple.
 John works as Software Engineer at Facebook.
 Steve works as Sales at Facebook.
 Ivan works as Sales at Apple.
 Zhang works as Software Engineer at Facebook.
 Chen works as Software Engineer at Facebook.
 Zhao works as Software Engineer at Facebook.
-`,
+Wang works as Engineering Manager at Facebook.
+Li works as Engineering Manager at Apple.
+You works as unknown at Lit Protocol.
+1kx works as investor at Lit Protocol.
+David Sneider works as founder at Lit Protocol.
+`;
+const responseJson = {
+  "credits": 1000,
+  "data": data,
 }
 const responseJsonError = {
   "credits": 3,
